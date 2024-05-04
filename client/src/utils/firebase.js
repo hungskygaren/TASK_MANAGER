9d2ad88c85b5dde5bd3b1e616ad89975631cd63a
LAPTOP-J35KI0IE\admin <hungskygaren@gmail.com>
2024-05-04 17:38:42 +0700

    all

---
+++ client/src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration 
+const firebaseConfig = {
+  apiKey: "AIzaSyAx8n9zV0BUqIBu_YDofIM8uZVyYSULhsk",
+  authDomain: "taskmanager-a56f3.firebaseapp.com",
+  projectId: "taskmanager-a56f3",
+  storageBucket: "taskmanager-a56f3.appspot.com",
+  messagingSenderId: "633317809159",
+  appId: "1:633317809159:web:7e8a0d5359307695a11ea2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- server/middlewares/errorMiddlewaves.js
+++ server/middlewares/errorMiddlewaves.js
@@ -3,7 +3,7 @@ const routeNotFound = (req, res, next) => {
   res.status(404);
   next(error);
 };
-
+const my = "string";
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
