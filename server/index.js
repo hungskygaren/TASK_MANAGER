88246b64d934424908445884c22967e6f7a5488a
Đinh Việt Hùng <93829944+hungskygaren@users.noreply.github.com>
2024-05-07 23:57:08 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://timely-axolotl-3223f3.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
